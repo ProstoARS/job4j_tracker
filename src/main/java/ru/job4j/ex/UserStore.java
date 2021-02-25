2b8e799c59f80c1cda0aa574d301c30974fee391
ProstoARS <arsudakov@gmail.com>
2021-02-25 09:49:37 +0300

    0.5. Иерархия исключений и множественный catch. [#459784]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -6,6 +6,7 @@ public class UserStore {
         for(User str : users) {
             if(str.getUsername().equals(login)) {
                 rsl = str;
+                break;
             }
         }
         if(rsl == null) {
